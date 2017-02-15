08cf7b8bd484a25c8c77889d0a9c46ad73ada200
owen <owen@oms.sh>
2017-02-14 20:03:02 -0500

    fix config

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+// ROCKET_ENV=dev|prod|stage ./wobsite
 #![feature(plugin)]
 #![plugin(rocket_codegen)]
 
@@ -6,7 +7,7 @@ extern crate rocket;
 use std::io;
 use std::path::{Path, PathBuf};
 use rocket::response::NamedFile;
-//use rocket::config::{Config, Environment};
+use rocket::config::{Config, Environment};
 
 
 #[get("/file/<file..>")]
@@ -25,14 +26,6 @@ fn not_found() -> Option<NamedFile> {
 }
 
 fn main() {
-    //let config = Config::build(Environment::Production)
-    //    .address("0.0.0.0")
-    //    .port(8070)
-    //    .finalize()
-    //    .unwrap()
-    //    ;
-    //let app = rocket::custom(config, false);
-    //app.mount("/", routes![hello, hi]).launch();
     rocket::ignite()
         .mount("/", routes![index, files])
         .catch(errors![not_found])
