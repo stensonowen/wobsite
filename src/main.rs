46bc93e54b7ab295706fabe094ae5f542fde7eab
owen <owen@oms.sh>
2017-02-15 14:57:35 -0500

    fixed some links

--- src/main.rs
+++ src/main.rs
@@ -6,27 +6,53 @@ extern crate rocket;
 
 use std::io;
 use std::path::{Path, PathBuf};
-use rocket::response::NamedFile;
+use rocket::response::{NamedFile, Redirect};
 
 
-#[get("/files/<file..>")]
-fn files(file: PathBuf) -> Option<NamedFile> {
-	NamedFile::open(Path::new("public/").join(file)).ok()
-}
+// BASIC ROUTES
 
 #[get("/")]
 fn index() -> io::Result<NamedFile> {
     NamedFile::open("static/index.html")
 }
 
+#[get("/files/<file..>")]
+fn files(file: PathBuf) -> Option<NamedFile> {
+	NamedFile::open(Path::new("public/").join(file)).ok()
+}
+
+#[get("/gpg")] fn gpg() -> io::Result<NamedFile> {
+    NamedFile::open("static/oms.gpg")
+}
+
+#[get("/resume")]
+fn resume() -> io::Result<NamedFile> {
+    NamedFile::open("static/resume.pdf")
+}
+
+#[get("/github")]
+fn github() -> Redirect {
+    Redirect::to("https://github.com/stensonowen")
+}
+
+
+
+// ERROR HANDLING
+
 #[error(404)]
 fn not_found() -> Option<NamedFile> {
 	NamedFile::open("static/404.png").ok()
 }
 
+
 fn main() {
+    //verify we're in the right directory
+    assert!(Path::new("static").is_dir(), "No `static` folder; change directories");
+    assert!(Path::new("public").is_dir(), "No `public` folder; change directories");
+    
+    //run
     rocket::ignite()
-        .mount("/", routes![index, files])
+        .mount("/", routes![index, files, gpg, resume, github])
         .catch(errors![not_found])
         .launch();
 }
